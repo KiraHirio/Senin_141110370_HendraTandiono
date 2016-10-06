2b7c7922fe37818611fac471e8ee01cbb847d154
Ghendra <happy.hendratandiono@gmail.com>
2016-10-06 11:26:46 +0700

    Latihan 2 First Try

---
+++ Latihan_2_1/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Latihan_2_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MC1 = new System.Windows.Forms.MonthCalendar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.UD2 = new System.Windows.Forms.DomainUpDown();
+            this.UD1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.UD1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MC1
+            // 
+            this.MC1.CalendarDimensions = new System.Drawing.Size(2, 2);
+            this.MC1.FirstDayOfWeek = System.Windows.Forms.Day.Saturday;
+            this.MC1.Location = new System.Drawing.Point(18, 114);
+            this.MC1.MaxSelectionCount = 3;
+            this.MC1.Name = "MC1";
+            this.MC1.TabIndex = 0;
+            this.MC1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.ChangeBold);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(211, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 58);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Bold";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Change);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(361, 44);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 58);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Un-Bold";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Reset);
+            // 
+            // UD2
+            // 
+            this.UD2.Items.Add("January");
+            this.UD2.Items.Add("February");
+            this.UD2.Items.Add("March");
+            this.UD2.Items.Add("April");
+            this.UD2.Items.Add("May");
+            this.UD2.Items.Add("June");
+            this.UD2.Items.Add("July");
+            this.UD2.Items.Add("August");
+            this.UD2.Items.Add("September");
+            this.UD2.Items.Add("October");
+            this.UD2.Items.Add("November");
+            this.UD2.Items.Add("December");
+            this.UD2.Location = new System.Drawing.Point(38, 82);
+            this.UD2.Name = "UD2";
+            this.UD2.Size = new System.Drawing.Size(120, 20);
+            this.UD2.TabIndex = 6;
+            this.UD2.Text = "Month";
+            this.UD2.SelectedItemChanged += new System.EventHandler(this.MonthChange);
+            // 
+            // UD1
+            // 
+            this.UD1.Location = new System.Drawing.Point(38, 44);
+            this.UD1.Maximum = new decimal(new int[] {
+            31,
+            0,
+            0,
+            0});
+            this.UD1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.UD1.Name = "UD1";
+            this.UD1.Size = new System.Drawing.Size(120, 20);
+            this.UD1.TabIndex = 7;
+            this.UD1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 443);
+            this.Controls.Add(this.UD1);
+            this.Controls.Add(this.UD2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.MC1);
+            this.Name = "Form1";
+            this.Text = "Latihan_2_1";
+            ((System.ComponentModel.ISupportInitialize)(this.UD1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar MC1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DomainUpDown UD2;
+        private System.Windows.Forms.NumericUpDown UD1;
+    }
+}
+
---
+++ Latihan_2_1/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Latihan_2_1
+{
+    public partial class Form1 : Form
+    {
+        public string me = "", me1 = "", me2 = "", me3 = "";
+        public void bold(DateTime Date1){
+            me1 = Date1.Day.ToString();
+            me2 = Date1.Month.ToString();
+            me3 = Date1.Year.ToString();
+            me = me2 + "/" + me1 + "/" + me3;
+            MC1.AddBoldedDate(Convert.ToDateTime(me));
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            boldme();
+        }
+
+        private void Change(object sender, EventArgs e)
+        {
+            if (UD2.SelectedIndex == -1) { MessageBox.Show("Please Choose Month Before Bold The Date"); return; }
+            string Day0 = UD1.Value.ToString(), Month0 = (UD2.SelectedIndex+1).ToString(), Year0 = DateTime.Today.Year.ToString();
+            DateTime Date0 = Convert.ToDateTime(Month0 + "/" + Day0 + "/" + Year0);
+            MC1.AddAnnuallyBoldedDate(Date0);
+            MC1.UpdateBoldedDates();
+        }
+        private void ChangeBold(object sender, EventArgs e)
+        {
+            boldme();
+        }
+        public void boldme()
+        {
+            DateTime DT1 = MC1.SelectionStart.AddMonths(-4);
+            DateTime DT2 = MC1.SelectionStart.AddMonths(4);
+            string Day1 = "01", Month1 = DT1.Month.ToString(), Year1 = DT1.Year.ToString(),
+                   Day2 = ""  , Month2 = DT2.Month.ToString(), Year2 = DT2.Year.ToString();
+
+            for(int i = 1; i <= 12 ; i++ ){if (DT2.Month == i){
+               if (i == 1 || i == 3 || i == 5 || i == 7 || i == 8 || i == 10 || i == 12) { Day2 = "31"; }
+               else if (i == 2) { Day2 = "28"; } else { Day2 = "30"; } break;
+            }  }
+
+            DateTime Date1 = Convert.ToDateTime(Month1+"/"+Day1+"/"+Year1);
+            DateTime Date2 = Convert.ToDateTime(Month2+"/"+Day2+"/"+Year2);
+
+            while(Date1 <= Date2){
+                if (Date1.DayOfWeek == DayOfWeek.Sunday){  bold(Date1);}
+                else if (Date1.DayOfWeek == DayOfWeek.Saturday) { bold(Date1); }
+                else if (Date1.Day == 2 && Date1.Month == 10) { bold(Date1); }
+                Date1 = Date1.AddDays(1);
+            }
+        }
+
+        private void Reset(object sender, EventArgs e)
+        {
+            string Day0 = UD1.Value.ToString(), Month0 = (UD2.SelectedIndex + 1).ToString(), Year0 = DateTime.Today.Year.ToString();
+            DateTime Date0 = Convert.ToDateTime(Month0 + "/" + Day0 + "/" + Year0);
+            MC1.RemoveAnnuallyBoldedDate(Date0);
+            MC1.UpdateBoldedDates();
+        }
+
+        private void MonthChange(object sender, EventArgs e)
+        {
+            int i = UD2.SelectedIndex+1;
+            if(i == 1 || i == 3 || i == 5 || i == 7 || i == 8 || i == 10 || i == 12){ UD1.Maximum = 31; }
+            else if(i == 2 ){ UD1.Maximum = 29;}
+            else {UD1.Maximum = 30;}
+        }
+     }
+}
